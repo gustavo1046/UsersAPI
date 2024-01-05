dc1994e51e9e14f0b7dbfe473115939e820d51fb
Gustavo <gustavo1046@hotmail.com>
2024-01-05 16:04:05 -0300

    Adicionando função de Post na API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app = express()
-const port = 300
+app.use(express.json())
+const port = 3000
+
 
 app.get('/', (res,req)=>{
     res.send("Hello world");
 })
 
+const User = mongoose.model('User', {
+    Name: String,
+    Email: String,
+    Password: String,
+    Birth: Date
+})
+
+app.post("/", async (req, res) =>{
+    const user = new User({
+        Name: req.body.Name,
+        Email: req.body.Email,
+        Password: req.body.Password,
+        Birth: req.body.Birth
+    })
+
+    await user.save()
+    res.send(user)
+})
+
+
+
 app.listen(port, ()=>{
+    mongoose.connect('mongodb+srv://songustavo17:937GoRtTFlbxQTCs@usersapi.0juhwku.mongodb.net/?retryWrites=true&w=majority')
     console.log("App running")
 })
