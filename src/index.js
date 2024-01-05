f807e161220b40dd8eec416034450e0e69cecb88
Gustavo <gustavo1046@hotmail.com>
2024-01-05 15:35:26 -0300

    Adicionando biblioteca Express

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const express =require('express');
+const express = require('express');
 const app = express()
-const port = 3000
+const port = 300
 
-app.get('/', (req, res)=>{
-    res.send("Hello World")
+app.get('/', (res,req)=>{
+    res.send("Hello world");
 })
 
 app.listen(port, ()=>{
-    console.log('App running')
+    console.log("App running")
 })
